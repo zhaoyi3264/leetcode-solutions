941d61c463f49ba14081bb10c52c30ab3dbdb6f7
zhaoyi3264 <zhaoyi3264@gmail.com>
2021-03-19 22:44:20 -0500

    add the first solution

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # leetcode-solutions
 My LeetCode solutions
+
+# Table of Contents
+- [Linked List](#linked-list)
+
+# Linked List
+- [1290. Convert Binary Number in a Linked List to Integer](src/1290.py)
---
+++ src/1290.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        ans = 0
+        cur = head
+        while cur:
+            ans = (ans << 1 | cur.val)
+            cur = cur.next
+        return ans
